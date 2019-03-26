3984e10376da52129b718e1723599e4c396de972
Adis Davletov <adavletov94@gmail.com>
2019-03-26 18:24:07 +0300

    lalallal bilstmclf

--- bert/metrics.py
+++ bert/metrics.py
@@ -31,3 +31,21 @@ def category_accuracy(gold_df, predicted_df, category='case'):
     correct_sentence *= 100
     correct *= 100
     return correct / total, correct_sentence / total_sentence
+
+def pos_accuracy(gold_df, predicted_df):
+    gold = gold_df.POSs.copy()
+    predicted = predicted_df.POSs.copy()
+    total, correct = 0, 0
+    total_sentence, correct_sentence = 0, 0
+    for sentence1, sentence2 in zip(gold, predicted):
+        flag = True
+        for pos1, pos2 in zip(sentence1, sentence2):
+            check = pos1 == pos2
+            if not check: flag = False
+            correct += int(check)
+            total += 1
+        correct_sentence += int(flag)
+        total_sentence += 1
+    correct_sentence *= 100
+    correct *= 100
+    return correct / total, correct_sentence / total_sentence
